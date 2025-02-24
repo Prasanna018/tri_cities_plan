f8d3a08b13541023f43d199d8f0ceacf6a2c3cf3
Prasanna Gaikwad <prasannagaikwad0018@gmail.com>
2025-02-24 13:49:39 +0530

    font change

--- src/Components/Employees.jsx
+++ src/Components/Employees.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function Employees() {
     return (
         <div className='p-2 space-y-4 bg-[#1e2a3a]
@@ -7,7 +8,7 @@ function Employees() {
             <div className='pb-8'>
                 <span className='font-bold text-3xl'>Employees</span>
             </div>
-            <p className='text-lg text-slate-300 '>
+            <p className=' text-slate-300 '>
                 The TCAMPO staff complete all of the federal and state requirements to allow the local member communities to spend federal and state transportation funding. Staff provide professional planning services through the development of the Regional Transportation Plan including sub-plans for transit, active transportation, and Congestion Management Process. The staff are also responsible for the ongoing administration of the region’s Transportation Improvement Program and periodic Call for Projects for federal funding for the region.
             </p>
 
@@ -15,7 +16,8 @@ function Employees() {
                 TRANSPORTATION PLANNING
 
             </h1>
-            <div className='flex flex-col text-xl pl-4 text-slate-300'>
+
+            <div className='flex flex-col  pl-4 text-slate-300'>
                 <span>
                     1.  Zak Mumuni: Assistant Director of Transportation – (804) 861-1666
                 </span>
